49bee792be754302b42a9f3f3cf52ca82d876bf2
diddyjax19 <adetobi59@gmail.com>
2023-04-27 13:25:30 +0000

    installed 3 dependecies and made changes to the axiosInstance.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Routing from './router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./components/custom.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I am actually learning React
-        </a>
-      </header>
-    </div>
+      <>
+          <ToastContainer />
+          <div>
+              <Routing />
+          </div>
+      </>
   );
-}
+};
 
 export default App;
+
